05a63adfe633177c94ef26e33fe82cc715ba9389
xiaokaixi <1085509316@qq.com>
2020-06-14 20:38:09 +0800

    delete

--- temp/putdata/hadoop_data_put.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-date +%F" "%H:%M:%S
-hdfs dfs -rm -r /data_test/2018-10-01.log
-hdfs dfs -put /software/temp/putdata/2018-10-01.log /data_test
-date +%F" "%H:%M:%S
-echo "put hadoop hdfs data success !"
--- temp/sort/mapper.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import sys
-list1 = []
-for line in sys.stdin:
-    line = line.strip()
-    words = line.split("\n")
-    list1.append(words[0])
-for x in range(len(list1)):
-    print(list1[x])
